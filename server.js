7b865ec20a2a2a105e6e6e6aff073e37b51a6d8b
Mangaldeep Das <rockonliku88@gmail.com>
2023-10-22 18:18:42 +0530

    Port change Commit

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.post('/generate', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
